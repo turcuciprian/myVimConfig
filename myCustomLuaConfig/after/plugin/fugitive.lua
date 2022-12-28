8d2c933d5f751281251a6614d6b40a7b95275ae4
turcuciprian <turcuciprian1@gmail.com>
2022-12-28 07:34:25 +0200

    added return, push and pull keymaps

--- myCustomLuaConfig/after/plugin/fugitive.lua
+++ myCustomLuaConfig/after/plugin/fugitive.lua
@@ -1,3 +1,5 @@
 vim.keymap.set("n",  "<leader>gs", vim.cmd.Git)
-vim.keymap.set("n",  "<leader>ga", (':Git add .'))
+vim.keymap.set("n",  "<leader>ga", (':Git add .\n'))
 vim.keymap.set("n",  "<leader>gc", (':Git commit -am "'))
+vim.keymap.set("n",  "<leader>gp", (':Git push\n'))
+vim.keymap.set("n",  "<leader>gP", (':Git pull\n'))
